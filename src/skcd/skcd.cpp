bb2b137f4cf2c65d7106902dd2a023dd24d226c5
Nathan Prat <nathan.prat@gmail.com>
2022-02-24 19:16:25 +0100

    skcd: avoid useless copies

--- src/skcd/skcd.cpp
+++ src/skcd/skcd.cpp
@@ -1,7 +1,6 @@
 #include "skcd.h"
 
 #include "skcd.pb.h"
-#include "utils/files/utils_files.h"
 
 namespace interstellar {
 
@@ -20,19 +19,19 @@ void WriteToFile(boost::filesystem::path path, const BlifParser &blif_parser) {
   skcd_pb.set_n(blif_parser.Getn());
   skcd_pb.set_q(blif_parser.Getq());
 
-  auto a = blif_parser.GetA();
+  const auto &a = blif_parser.GetA();
   google::protobuf::RepeatedField<uint32_t> a_data(a.begin(), a.end());
   skcd_pb.mutable_a()->Swap(&a_data);
 
-  auto b = blif_parser.GetB();
+  const auto &b = blif_parser.GetB();
   google::protobuf::RepeatedField<uint32_t> b_data(b.begin(), b.end());
   skcd_pb.mutable_b()->Swap(&b_data);
 
-  auto go = blif_parser.GetGO();
+  const auto &go = blif_parser.GetGO();
   google::protobuf::RepeatedField<uint32_t> go_data(go.begin(), go.end());
   skcd_pb.mutable_go()->Swap(&go_data);
 
-  auto gt = blif_parser.GetGT();
+  const auto &gt = blif_parser.GetGT();
   google::protobuf::RepeatedField<int32_t> gt_data;  // (gt.begin(), gt.end())
   gt_data.Reserve(gt.size());
   for (auto it : gt) {
@@ -40,15 +39,16 @@ void WriteToFile(boost::filesystem::path path, const BlifParser &blif_parser) {
   }
   skcd_pb.mutable_gt()->Swap(&gt_data);
 
-  auto o = blif_parser.GetO();
+  const auto &o = blif_parser.GetO();
   google::protobuf::RepeatedField<uint32_t> o_data(o.begin(), o.end());
   skcd_pb.mutable_o()->Swap(&o_data);
 
-  auto buf = skcd_pb.SerializeAsString();
-  if (buf.empty()) {
-    throw std::range_error("Serialization failed");
+  std::fstream output(path.generic_string(),
+                      std::ios::out | std::ios::trunc | std::ios::binary);
+  auto ok = skcd_pb.SerializeToOstream(&output);
+  if (!ok) {
+    throw std::runtime_error("Serialization failed");
   }
-  utils::WriteToFile(path, buf);
 }
 
 }  // namespace skcd
